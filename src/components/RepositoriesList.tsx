a147eca16922dbd2fa4b157e50ca19b928fb5858
peter_alfie <alfie1104@gmail.com>
2022-08-18 08:26:52 +0900

    feat(repositorieslist, reducers/index, index): selecting state and awkaward typing around reactredux

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { useActions } from "../hooks/useActions";
+import { RootState } from "../state";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useSelector(
+    (state: RootState) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
