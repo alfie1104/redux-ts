90ca39fed71a92a4b5e69cfe1519398da8be9fa6
peter_alfie <alfie1104@gmail.com>
2022-08-18 08:33:49 +0900

    feat(usetypedselector, repositorieslist): creating a typed selector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,13 +1,13 @@
+import { stat } from "fs";
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
 import { useActions } from "../hooks/useActions";
-import { RootState } from "../state";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
-  const { data, error, loading } = useSelector(
-    (state: RootState) => state.repositories
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
   );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
